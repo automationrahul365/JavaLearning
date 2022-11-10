1e1d7c96b7a8cd3f44c9a647dd80833e89aa9acf
Rahul Sharma <qa.rahul365@gmail.com>
2022-11-10 11:55:48 +0530

    Constructor assignnment

--- JavaLearning/src/package_1/Day3.java
+++ JavaLearning/src/package_1/Day3.java
@@ -2,7 +2,32 @@ package package_1;
 
 public class Day3 
 {
+	public Day3()
+	{
+		this(1,2,3,4);
+		System.out.println("Default constructor");
+	}
+	public Day3(int a)
+	{
+		this();
+		System.out.println("One parameterized constructor");
+	}
+	public Day3(int a, int b)
+	{
+		this(1,2,3);
+		System.out.println("Two parameterized constructor");
+	}
+	public Day3(int a, int b, int c)
+	{
+		this(1);
+		System.out.println("Three parameterized constructor");
+	}
+	public Day3(int a, int b, int c, int d)
+	{
+		
+		System.out.println("Four parameterized constructor");
+	}
 public static void main(String[] args) {
-	System.out.println("Hello World");
+	Day3 object= new Day3(3,3);
 }
 }
